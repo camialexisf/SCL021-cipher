043e5f9664db782e19b2301c28c0f2211b66787f
camialexisf <camialexisflores@gmail.com>
2022-07-07 13:26:43 -0400

    primer intento de añadir letras minusculas y ayusculas diferenciadas

--- src/cipher.js
+++ src/cipher.js
@@ -1,26 +1,40 @@
-
 const cipher = {
   encode: (text, offset) => {
-      if (!text)
-      {
-      return '';}
-      else{
-      let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    if (!text) {
+      return '';
+    }
+    else if (abc == abc.toUpperCase()) {
+      let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        offset = (offset % 26 + 26) % 26;
+      return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) + offset) % 26]);
+    }
+    else if (abc == abc.toLowerCase()) {
+      let abc = 'abcdefghijklmnopqrstuvwxyz';
       offset = (offset % 26 + 26) % 26;
-      return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) + offset)% 26]);}
+      return text.replace(/[a-z]/ig, c => abc[(abc.indexOf(c) + offset) % 26]);
+
+    }
   },
+
   decode: (text, offset) => {
-    if (!text)
-    {
-    return '';}
-    else{
-    let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    offset = (offset % 26 + 26) % 26;
-    return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) - offset)% 26]);}
-},
+    let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (!text) {
+      return '';
+    }
+    else if (abc == abc.toUpperCase()) {
+      let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        offset = (offset % 26 + 26) % 26;
+      return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) - offset) % 26]);
+    } 
+    else if (abc == abc.toLowerCase()) {
+      let abc = 'abcdefghijklmnopqrstuvwxyz';
+      offset = (offset % 26 + 26) % 26;
+      return text.replace(/[a-z]/ig, c => abc[(abc.indexOf(c) - offset) % 26]);
+    }}
+    }
 
-};
 
 
-
-export default cipher;
+    export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,58 +1,61 @@
 import cipher from './cipher.js';
 
+
+
+/* FUNCION Q HACE LINK AL BOTON QUE CODIFICA */
+document.getElementById('code').addEventListener('click', function () {
+    let text = document.getElementById('msg').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('decode-msg').value = cipher.encode(text, offset);
+}, true);
+
+/*FUNCION QUE  HACE EL LINK A BOTON Q DECODIFICA */
+document.getElementById('decode').addEventListener('click', function () {
+    let text = document.getElementById('msg').value;
+    let offset = document.getElementById('offset').value;
+    document.getElementById('decode-msg').value = cipher.decode(text, offset);
+}, true);
+
 console.log(cipher);
 
-window.addEventListener('load', inicio, true);
+/* FUNCION DEL BOTON QUE COPIA*/
 
-document.getElementById('copyText').addEventListener('click', function copy(){
-    var copyText= document.getElementById('decode-msg');
+document.getElementById('copyText').addEventListener('click', function copy() {
+    var copyText = document.getElementById('decode-msg');
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copyText.value)
-    alert('Se ha copiado el texto: '+ copyText.value)
+    alert('Se ha copiado el texto: ' + copyText.value)
 
 });
- document.getElementById('code').addEventListener('click', cipher.encode);
- function inicio(){
-    document.getElementById('msg').addEventListener('keyup', function(){
-        this.value = this.value.toUpperCase();
-    }, true);};
 
 
-              /*TRANSFORMA LETRAS A MAYUS
+/*TRANSFORMA LETRAS A MAYUS*/
 
 
-      /* FUNCION Q HACE LINK AL BOTON QUE CODIFICA */
-document.getElementById('code').addEventListener('click', function(){
-      let text = document.getElementById('msg').value;
-      let offset = document.getElementById('offset').value;
-    document.getElementById('decode-msg').value= cipher.encode(text, offset);
-     }, true);
 
-       /*FUNCION QUE  HACE EL LINK A BOTON Q DECODIFICA */
- document.getElementById('decode').addEventListener('click', function(){
-    let text = document.getElementById('msg').value;
-    let offset = document.getElementById('offset').value;
-    document.getElementById('decode-msg').value = cipher.decode(text,offset);
-}, true);
+   //document.getElementById('msg').addEventListener('keyup', function () {
+   //     this.value = this.value.toUpperCase();
+    //}, true);
+//};
 
-      /*FUNCION QUE CODIFICA
+/*FUNCION QUE CODIFICA
 /*function cifrar(text, offset){
-    if (!text)
-    {
-    return '';}
-    else{
-    let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    offset = (offset % 26 + 26) % 26;
-    return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) + offset)% 26]);}
+if (!text)
+{
+return '';}
+else{
+let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+offset = (offset % 26 + 26) % 26;
+return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) + offset)% 26]);}
 }
-            /*FUNCION QUE DECODIFICA
+      /*FUNCION QUE DECODIFICA
 function descifrar(text, offset){
-    if (!text)
-    return '';
-    let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    offset = (offset % 26 - 26) % 26;
-    return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) - offset)% 26]);
+if (!text)
+return '';
+let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+offset = (offset % 26 - 26) % 26;
+return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) - offset)% 26]);
 }
 */
 
