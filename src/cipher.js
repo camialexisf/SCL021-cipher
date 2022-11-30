37dd0d450031ca3efb3aceeda3adc8cb7e185277
camialexisf <camialexisflores@gmail.com>
2022-11-30 15:35:12 -0300

    se edito archivo css

--- src/cipher.js
+++ src/cipher.js
@@ -1,71 +1,38 @@
-
 const cipher = {
-   /*CIFRADO*/
-   encode: (offset,text) => {
-      if (typeof (offset) == 'number' || typeof (text) == 'string')  {
-         let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-          offset = (offset % 26 + 26) % 26;
-         /*FUNCION QUE HACE EL CIFRADO, SE AGREGA SUMA DE 26 PARA QUE PUEDA OPERAR CON OFFSET ALTOS O NEGATIVOS */
-         return text.replace(/[A-Z]/g, c => abc[(abc.indexOf(c) + offset + 26) % 26]);
+  /*CIFRADO*/
+  encode: (offset, text) => {
+    if (typeof offset == "number" || typeof text == "string") {
+      let result = "";
+      let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      offset = ((offset % 26) + 26) % 26;
+      //FUNCION QUE HACE EL CIFRADO, SE AGREGA SUMA DE 26 PARA QUE PUEDA OPERAR CON OFFSET ALTOS O NEGATIVOS //
+      // return text.replace(/[A-Z]/g, c => abc[(abc.indexOf(c) + offset + 26) % 26]);//
+      //FUNCION UTILIZANDO FOR//
+      for (let i = 0; i < text.length; i++) {
+        if (abc.indexOf(text[i] != -1)) {
+          let position = (abc.indexOf(text[i]) + offset) % 26;
+          result += abc[position];
+        } else result += text[i];
       }
-      /*TIRAR ERROR DE TIPO SI EL TIPO DE DATO INGRESADO NO CORRESPONDE*/
-      else if (typeof (offset) != 'number' || typeof (text) != 'string') {
-         throw new TypeError('Wrong argument types');
-      }
-
-
-
-   },
-   /*DESCIFRADO // LO MISMO DE ARRIBA PERO RESTANDO*/
-   decode: (offset, text) => {
-      if (typeof (offset) == 'number' ||typeof (text) == 'string') {
-         let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-          offset = (offset % 26 + 26) % 26;
-         return text.replace(/[A-Z]/g, c => abc[(abc.indexOf(c) - offset + 26) % 26]);
-
-      }
-      else if (typeof (offset) != 'number' || typeof (text) != 'string') {
-         throw new TypeError('Wrong argument types');
-      }
-
-
-
-   }
+      return result;
+    } else {
+    /*TIRAR ERROR DE TIPO SI EL TIPO DE DATO INGRESADO NO CORRESPONDE*/
+      throw new TypeError("Wrong argument types");
+    }
+  },
+  /*DESCIFRADO // LO MISMO DE ARRIBA PERO RESTANDO*/
+  decode: (offset, text) => {
+    if (typeof offset == "number" || typeof text == "string") {
+      let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      offset = ((offset % 26) + 26) % 26;
+      return text.replace(
+        /[A-Z]/g,
+        (c) => abc[(abc.indexOf(c) - offset + 26) % 26]
+      );
+    } else {
+      throw new TypeError("Wrong argument types");
+    }
+  },
 };
-
-/*
-const cipher = {
-   /*CIFRADO
-   encode: (offset,text) => {
-      if (offset, text)  {
-         let ABC = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-          offset = (offset % 26 + 26) % 26;
-         /*FUNCION QUE HACE EL CIFRADO, SE AGREGA SUMA DE 26 PARA QUE PUEDA OPERAR CON OFFSET ALTOS O NEGATIVOS 
-         return text.replace(/[A-Za-z]/g, c => ABC[(ABC.indexOf(c) + offset) % 52]);
-      }
-      TIRAR ERROR DE TIPO SI EL TIPO DE DATO INGRESADO NO CORRESPONDE
-      else if (typeof (offset) != 'number' || typeof (text) != 'string') {
-         throw new TypeError('Wrong argument types');
-      }
-      DESCIFRADO  LO MISMO DE ARRIBA PERO RESTANDO
-   decode: (offset, text) => {
-      if (typeof (text) == 'string' || typeof (offset) == 'number') {
-         let abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-          offset = (offset % 26 + 26) % 26;
-         return text.replace(/[A-Za-z]/g, c => abc[(abc.indexOf(c) - offset) % 52]);
-
-      }
-      else if (typeof (offset) != 'number' || typeof (text) != 'string') {
-         throw new TypeError('Wrong argument types');
-      }
-
-
-
-   }
-};*/
-
-
-
-
 
 export default cipher;
