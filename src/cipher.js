88baf9e0ff18063a27f208749837a90646b4600b
camialexisf <camialexisflores@gmail.com>
2022-12-21 18:45:16 -0300

    el cifrado ahora funciona con espacios, tambien se vuelve responsive por medio de  mediaQuerys

--- src/cipher.js
+++ src/cipher.js
@@ -1,37 +1,40 @@
 const cipher = {
   /*CIFRADO*/
   encode: (offset, text) => {
-    if (typeof offset == "number" || typeof text == "string") {
-      let result = "";
-      let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      offset = ((offset % 26) + 26) % 26;
-      //FUNCION QUE HACE EL CIFRADO, SE AGREGA SUMA DE 26 PARA QUE PUEDA OPERAR CON OFFSET ALTOS O NEGATIVOS //
-      // return text.replace(/[A-Z]/g, c => abc[(abc.indexOf(c) + offset + 26) % 26]);//
-      //FUNCION UTILIZANDO FOR//
-      for (let i = 0; i < text.length; i++) {
-        if (abc.indexOf(text[i] != -1)) {
-          let position = (abc.indexOf(text[i]) + offset) % 26;
-          result += abc[position];
-        } else result += text[i];
+    let result = "";
+    let toEncrypt = text.toUpperCase();
+
+    for (let i = 0; i < text.length; i++) {
+      const letter = toEncrypt[i].charCodeAt();
+
+      if (toEncrypt[i] == " ") {
+        result = result + " ";
+      } else {
+        let encyptedLetter = String.fromCharCode(
+          ((letter - 65 + parseInt(offset)) % 26) + 65
+        );
+        result = result + encyptedLetter;
       }
-      return result;
-    } else {
-    /*TIRAR ERROR DE TIPO SI EL TIPO DE DATO INGRESADO NO CORRESPONDE*/
-      throw new TypeError("Wrong argument types");
     }
+    return result;
   },
-  /*DESCIFRADO // LO MISMO DE ARRIBA PERO RESTANDO*/
   decode: (offset, text) => {
-    if (typeof offset == "number" || typeof text == "string") {
-      let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      offset = ((offset % 26) + 26) % 26;
-      return text.replace(
-        /[A-Z]/g,
-        (c) => abc[(abc.indexOf(c) - offset + 26) % 26]
-      );
-    } else {
-      throw new TypeError("Wrong argument types");
+    let result = "";
+    let toEncrypt = text.toUpperCase();
+
+    for (let i = 0; i < text.length; i++) {
+      const letter = toEncrypt[i].charCodeAt();
+
+      if (toEncrypt[i] == " ") {
+        result = result + " ";
+      } else {
+        let encyptedLetter = String.fromCharCode(
+          ((letter + 65 - parseInt(offset)) % 26) + 65
+        );
+        result = result + encyptedLetter;
+      }
     }
+    return result;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,59 +1,44 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
 /* BOTON QUE CODIFICA */
-document.getElementById('code').addEventListener('click', function () {
-    let text = document.getElementById('msg').value;
-    let offset = (parseInt(document.getElementById('offset').value));
-    document.getElementById('decode-msg').value = cipher.encode(offset, text);
-}, true);
+document.getElementById("code").addEventListener(
+  "click",
+  () => {
+    let text = document.getElementById("msg").value;
+    let offset = parseInt(document.getElementById("offset").value);
+    document.getElementById("decode-msg").value = cipher.encode(offset, text);
+  },
+  true
+);
 
 /*BOTON QUE DECODIFICA */
-document.getElementById('decode').addEventListener('click', function () {
-    let text = document.getElementById('msg').value;
-    let offset = (parseInt(document.getElementById('offset').value));
-    document.getElementById('decode-msg').value = cipher.decode(offset, text);
-}, true);
-
-console.log(cipher);
+document.getElementById("decode").addEventListener(
+  "click",
+  function () {
+    let text = document.getElementById("msg").value;
+    let offset = parseInt(document.getElementById("offset").value);
+    document.getElementById("decode-msg").value = cipher.decode(offset, text);
+  },
+  true
+);
 
 /*BOTON QUE COPIA*/
 
-document.getElementById('copyText').addEventListener('click', function copy() {
-    var copyText = document.getElementById('decode-msg');
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value)
-    alert('Se ha copiado el texto: ' + copyText.value)
-
+document.getElementById("copyText").addEventListener("click", function copy() {
+  var copyText = document.getElementById("decode-msg");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
+  alert("Se ha copiado el texto: " + copyText.value);
 });
-
 
 /*TRANSFORMA LETRAS A MAYUS*/
 
-
-document.getElementById('msg').addEventListener('keyup', function () {
-     this.value = this.value.toUpperCase();
-    }, true);
+document.getElementById("msg").addEventListener(
+  "keyup",
+  function () {
+    this.value = this.value.toUpperCase();
+  },
+  true
+);
 //
-
-
-/*FUNCION QUE CODIFICA
-/*function cifrar(text, offset){
-if (!text)
-{
-return '';}
-else{
-let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-offset = (offset % 26 + 26) % 26;
-return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) + offset)% 26]);}
-}
-      /*FUNCION QUE DECODIFICA
-function descifrar(text, offset){
-if (!text)
-return '';
-let abc= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-offset = (offset % 26 - 26) % 26;
-return text.replace(/[A-Z]/ig, c => abc[(abc.indexOf(c) - offset)% 26]);
-}
-*/
-
